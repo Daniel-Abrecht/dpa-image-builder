5b08e094eefffd63de4057378c94a8cf4478697e
Daniel Abrecht <public@danielabrecht.ch>
2018-11-18 23:41:33 +0000

    Another small bugfix

--- src/repositories.mk
+++ src/repositories.mk
@@ -35,11 +35,13 @@ repo/linux/.repo:
 	touch "$@"
 
 # TODO: get rid of this firmware crap, it's not FOSS!!!
-repo/firmware-imx/.repo:
-	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-7.2.bin -O "$@".tmp
-	if [ "$$(sha256sum "$@".tmp | grep -o '^[^ ]*')" != 3e107d83ed2367c9565250d6ff3903cc604bf4d9aa505391260ead0f51ceb8d9 ]; then \
+repo/firmware-imx/.repo: repo/firmware-imx/.dir
+	set -e; \
+	file="repo/firmware-imx/firmware-imx-7.2.bin"; \
+	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-7.2.bin -O "$$file.tmp"; \
+	if [ "$$(sha256sum "$$file".tmp | grep -o '^[^ ]*')" != 3e107d83ed2367c9565250d6ff3903cc604bf4d9aa505391260ead0f51ceb8d9 ]; then \
 	  echo "Checksum mismatch, file firmware-imx-7.2.bin was modified by someone" >&2; \
 	  false; \
-	fi
-	mv "$@".tmp "$@"
+	fi; \
+	mv "$$file".tmp "$$file";
 	touch "$@"
