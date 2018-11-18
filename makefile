1cc8dec9d373984e9bb684a5c17639a5b996c409
Daniel Abrecht <public@danielabrecht.ch>
2018-11-18 23:14:04 +0000

    A few small bugfixes

--- makefile
+++ makefile
@@ -79,5 +79,5 @@ clean-build:
 	make -C kernel clean-build
 	rm -rf bin/ build/
 
-emulate: bin/$(IMAGE_NAME)
-	qemu-system-aarch64 -M virt -cpu cortex-a53 -m 3G -kernel kernel/linux/debian/tmp/boot/vmlinuz-* -append "root=/dev/vda3" -drive if=none,file=bin/$(IMAGE_NAME),format=raw,id=hd -device virtio-blk-device,drive=hd -nographic
+emulate: bin/$(IMAGE_NAME) kernel/bin/linux-image.deb
+	qemu-system-aarch64 -M virt -cpu cortex-a53 -m 3G -kernel repo/linux/debian/tmp/boot/vmlinuz-* -append "root=/dev/vda3" -drive if=none,file=bin/$(IMAGE_NAME),format=raw,id=hd -device virtio-blk-device,drive=hd -nographic
--- src/repositories.mk
+++ src/repositories.mk
@@ -1,31 +1,31 @@
 
-repo/fuseloop/.repo: build/.dir
+repo/fuseloop/.repo:
 	git clone https://github.com/jmattsson/fuseloop.git repo/fuseloop
 	touch "$@"
 
-repo/usernsexec/.repo: build/.dir
+repo/usernsexec/.repo:
 	git clone https://github.com/Daniel-Abrecht/usernsexec.git repo/usernsexec
 	touch "$@"
 
-repo/tar2ext/.repo: build/.dir
+repo/tar2ext/.repo:
 	git clone https://github.com/Daniel-Abrecht/tar2ext.git repo/tar2ext
 	touch "$@"
 
-repo/uboot-imx/.repo: repo/.dir
+repo/uboot-imx/.repo:
 	git clone -b pureos-patches https://source.puri.sm/Librem5/uboot-imx.git "repo/uboot-imx/"
 	touch "$@"
 
-repo/Cortex_M4/.repo: repo/.dir
+repo/Cortex_M4/.repo:
 	#git clone git@code.puri.sm:Angus_Ainslie/Cortex_M4.git # That was probably an internal repo
 	git clone https://source.puri.sm/Librem5/Cortex_M4.git "repo/Cortex_M4/"
 	touch "$@"
 
-repo/arm-trusted-firmware/.repo: repo/.dir
+repo/arm-trusted-firmware/.repo:
 	#git clone https://github.com/ARM-software/arm-trusted-firmware.git # TODO: Check this out from master once imx8mq gets merged.
 	git clone -b imx_4.9.51_imx8m_beta https://source.codeaurora.org/external/imx/imx-atf "repo/arm-trusted-firmware/"
 	touch "$@"
 
-repo/imx-mkimage/.repo: repo/.dir
+repo/imx-mkimage/.repo:
 	git clone https://source.codeaurora.org/external/imx/imx-mkimage -b imx_4.9.51_imx8m_beta "repo/imx-mkimage/"
 	touch "$@"
 
@@ -35,7 +35,7 @@ repo/linux/.repo:
 	touch "$@"
 
 # TODO: get rid of this firmware crap, it's not FOSS!!!
-repo/firmware-imx/.repo: repo/firmware-imx/.dir
+repo/firmware-imx/.repo:
 	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-7.2.bin -O "$@".tmp
 	if [ "$$(sha256sum "$@".tmp | grep -o '^[^ ]*')" != 3e107d83ed2367c9565250d6ff3903cc604bf4d9aa505391260ead0f51ceb8d9 ]; then \
 	  echo "Checksum mismatch, file firmware-imx-7.2.bin was modified by someone" >&2; \
