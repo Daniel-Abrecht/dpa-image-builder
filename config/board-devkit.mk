881661d7c5c023c16c56385a925a0d17abcb2f69
Daniel Abrecht <public@danielabrecht.ch>
2019-06-27 21:07:35 +0200

    Update kernel repo url & dtb

--- config/board-devkit.mk
+++ config/board-devkit.mk
@@ -5,11 +5,11 @@ UBOOT_DTB = librem5-evk.dtb
 repo-branch@uboot = devkit-wip
 repo-source@uboot = https://source.puri.sm/Librem5/uboot-imx.git
 
-KERNEL_CONFIG_TARGET = librem5-evk
-KERNEL_DTB = freescale/librem5-evk.dtb
+KERNEL_CONFIG_TARGET = librem5-devkit
+KERNEL_DTB = freescale/imx8mq-librem5-devkit.dtb
 
-repo-branch@linux = imx8-4.18-wip
-repo-source@linux = https://source.puri.sm/Librem5/linux-emcraft.git
+repo-branch@linux = imx8-5.2-devkit
+repo-source@linux = https://source.puri.sm/Librem5/linux-next.git
 
 repo-branch@imx-mkimage = imx-mkimage-emcraft
 repo-source@imx-mkimage = ../../.git
