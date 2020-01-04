cf29521b6c042604fb12e0c66fdbbf117b154dfd
Daniel Abrecht <public@danielabrecht.ch>
2020-01-04 03:37:31 +0100

    Use newer SOC firmware, make flash scripts work with newer SOC, fix interference with uboot make file

--- src/repositories.mk
+++ src/repositories.mk
@@ -2,10 +2,10 @@
 # TODO: get rid of this firmware crap, it's not FOSS!!!
 repo/.firmware-imx.repo: repo/firmware-imx/.dir
 	set -e; \
-	file="repo/firmware-imx/firmware-imx-7.2.bin"; \
-	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-7.2.bin -O "$$file.tmp"; \
-	if [ "$$(sha256sum "$$file".tmp | grep -o '^[^ ]*')" != 3e107d83ed2367c9565250d6ff3903cc604bf4d9aa505391260ead0f51ceb8d9 ]; then \
-	  echo "Checksum mismatch, file firmware-imx-7.2.bin was modified by someone" >&2; \
+	file="repo/firmware-imx/firmware-imx-7.9.bin"; \
+	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-7.9.bin -O "$$file.tmp"; \
+	if [ "$$(sha256sum "$$file".tmp | grep -o '^[^ ]*')" != 30e22c3e24a8025d60c52ed5a479e30fad3ad72127c84a870e69ec34e46ea8c0 ]; then \
+	  echo "Checksum mismatch, file firmware-imx-7.9.bin was modified by someone" >&2; \
 	  false; \
 	fi; \
 	mv "$$file".tmp "$$file";
