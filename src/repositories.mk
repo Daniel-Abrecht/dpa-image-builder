e889c68ab178463dcafcab42d9febe91675ff870
Daniel Abrecht <public@danielabrecht.ch>
2020-08-16 13:51:55 +0000

    Update NXP firmware

--- src/repositories.mk
+++ src/repositories.mk
@@ -2,10 +2,10 @@
 # TODO: get rid of this firmware crap, it's not FOSS!!!
 repo/.firmware-imx.repo: repo/firmware-imx/.dir
 	set -e; \
-	file="repo/firmware-imx/firmware-imx-7.9.bin"; \
-	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-7.9.bin -O "$$file.tmp"; \
-	if [ "$$(sha256sum "$$file".tmp | grep -o '^[^ ]*')" != 30e22c3e24a8025d60c52ed5a479e30fad3ad72127c84a870e69ec34e46ea8c0 ]; then \
-	  echo "Checksum mismatch, file firmware-imx-7.9.bin was modified by someone" >&2; \
+	file="repo/firmware-imx/firmware-imx-8.7.bin"; \
+	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-8.7.bin -O "$$file.tmp"; \
+	if [ "$$(sha256sum "$$file".tmp | grep -o '^[^ ]*')" != 92c1713f61a99b1ff5046a795789e6021db1e8bb5534c02e4b719f1436e15615 ]; then \
+	  echo "Checksum mismatch, file firmware-imx-8.7.bin was modified by someone" >&2; \
 	  false; \
 	fi; \
 	mv "$$file".tmp "$$file";
