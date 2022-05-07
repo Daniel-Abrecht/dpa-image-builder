9482cd05e4f06279d625dbf9b13d45c26e77bdea
Daniel Abrecht <public@danielabrecht.ch>
2022-05-07 13:25:30 +0000

    Bugfix

--- script/debootstrap-base.sh
+++ script/debootstrap-base.sh
@@ -9,7 +9,7 @@ rootfs="$(realpath "$1")"; shift
 cd /tmp/
 
 DEBOOTSTRAP_DIR="$X_DEBOOTSTRAP_DIR/usr/share/debootstrap/" uexec "$X_DEBOOTSTRAP_DIR/usr/sbin/debootstrap" --foreign --arch=arm64 "$@" "$RELEASE" "$rootfs" "$REPO" "$DEBOOTSTRAP_SCRIPT"
-echo meow >"$rootfs/hostname" # Some things need a hostname, but we don't want to leak the hostname of the bootstrapping system
+echo meow >"$rootfs/etc/hostname" # Some things need a hostname, but we don't want to leak the hostname of the bootstrapping system
 mkdir -p "$rootfs/root/helper"
 echo '#!/bin/sh' >"$rootfs/root/helper/mknod" # Don't worry about this, on boot, the kernel mounts /dev as devtmpfs before calling init anyway
 chmod +x "$rootfs/root/helper/"*
