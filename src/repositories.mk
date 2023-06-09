d445e472d48393ce532c5a3a885f086c9ae11eec
Daniel Abrecht <public@danielabrecht.ch>
2023-06-09 22:51:32 +0000

    Update firmware

--- src/repositories.mk
+++ src/repositories.mk
@@ -2,10 +2,10 @@
 # TODO: get rid of this firmware crap, it's not FOSS!!!
 repo/.firmware-imx.repo: repo/firmware-imx/.dir
 	set -e; \
-	file="repo/firmware-imx/firmware-imx-8.10.bin"; \
-	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-8.10.bin -O "$$file.tmp"; \
-	if [ "$$(sha256sum "$$file".tmp | grep -o '^[^ ]*')" != 2b70f169d4065b2a7ac7a676afe24636128bd2dacc9f5230346758c3b146b2be ]; then \
-	  echo "Checksum mismatch, file firmware-imx-8.10.bin was modified by someone" >&2; \
+	file="repo/firmware-imx/firmware-imx-8.12.bin"; \
+	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-8.12.bin -O "$$file.tmp"; \
+	if [ "$$(sha256sum "$$file".tmp | grep -o '^[^ ]*')" != 6b6747bf36ecc53e385234afdce01f69c5775adf0d6685c885281ca6e4e322ef ]; then \
+	  echo "Checksum mismatch, file firmware-imx-8.12.bin was modified by someone" >&2; \
 	  false; \
 	fi; \
 	mv "$$file".tmp "$$file";
