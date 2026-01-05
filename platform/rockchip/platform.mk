b750cb637558df0c451c8377c9acef02a5859a8d
Daniel Abrecht <public@danielabrecht.ch>
2026-01-05 15:00:27 +0000

    WIP: update it all

--- platform/rockchip/platform.mk
+++ platform/rockchip/platform.mk
@@ -1,5 +1,5 @@
 export UBOOT_DIR = $(project_root)/platform/$(BUILDER_PLATFORM)/uboot/
-export BOOTLOADER_BIN = $(UBOOT_DIR)/bin/u-boot.bin
+export BOOTLOADER_BIN = $(UBOOT_DIR)/bin/$(BOARD)/u-boot.bin
 
 PLATFORM_FILES = $(BOOTLOADER_BIN)
 
